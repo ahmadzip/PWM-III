a74b8383aa9527bec5aee24cc90fa0ebca39d821
ahmadzip <ahmad.5220411084@student.uty.ac.id>
2025-10-24 15:00:14 +0700

    refactor: update home route to render index.html and add new template for data fetching

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
-
-@app.route('/about')
-def about():
-    return "About Flask!"
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
