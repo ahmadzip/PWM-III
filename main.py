2f5e44dbad146629b1ae1871377f4cbf497d075a
ahmadzip <ahmad.5220411084@student.uty.ac.id>
2025-10-24 14:02:59 +0700

    menambah halaman about

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route('/about')
+def about():
+    return "About Flask!"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
